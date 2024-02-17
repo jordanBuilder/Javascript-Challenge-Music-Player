c739ab485e2c3a9fa433c32268ebd9edfb993d5b
jordan Tomegah <tomegahjordan81@gmail.com>
2024-02-17 20:51:29 +0000

    creation des fonctions loadMusic et changeMusic et updateProgressBar et setProgressBar, projet terminé

--- code/index.js
+++ code/index.js
@@ -17,19 +17,19 @@ const songs = [
     {
         path : "../songs/The lumineers - Sleep on the floor.mp3",
         displayName: "Sleep on the floor",
-        cover: "The Lumineers Iphone Wallpaper-Sleep on the floor #lumineersaesthetic #lumineers #sleeponthefloor #thelumimeers.jpg",
+        cover: "../images/The_Lumineers_Sleep_On_The_Floor.jpg",
         artist: "The Lumineers",
     },
     {
         path : "../songs/The lumineers - Angela.mp3",
         displayName: "Angela",
-        cover: "the lumineers -- Angela.jpg",
+        cover: "../images/the lumineers -- Angela.jpg",
         artist: "The Lumineers",
     },
     {
         path : "../songs/The lumineers Cleopatra.mp3",
         displayName: "Cleopatra",
-        cover: "lumineers wallpaper.jpg",
+        cover: "../images/lumineers wallpaper.jpg",
         artist: "The Lumineers",
     },
 ];
@@ -49,7 +49,7 @@ function playMusic(){
     //Change play button icon
     playBtn.classList.replace("fa-play", "fa-pause");
     // set button hover title
-    playBtn.setAttribute('title','pause');
+    playBtn.setAttribute('title','Pause');
 
     music.play();
 }
@@ -60,7 +60,46 @@ function pauseMusic(){
     //Change play button icon
     playBtn.classList.replace("fa-pause", "fa-play");
     // set button hover title
-    playBtn.setAttribute('title','play');
-
+    playBtn.setAttribute('title','Play');
     music.pause();
 }
+
+function loadMusic(song){
+    music.src = song.path;
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    image.src = song.cover;
+    background.src = song.cover;
+}
+
+function changeMusic(direction){
+    musicIndex = (musicIndex + direction + songs.length) % songs.length;
+    loadMusic(songs[musicIndex]);
+    playMusic(); 
+}
+
+function updateProgressBar(){
+    const {duration, currentTime} = music;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+
+    const formatTime = (time) =>String(Math.floor(time)).padStart(2, '0');
+
+    durationEl.textContent = `${formatTime(duration/60)}:${formatTime(duration % 60)}`;
+    currentTimeEl.textContent = `${formatTime(currentTime/60)}:${formatTime(currentTime % 60)}`;
+}
+function setProgressBar(e){
+    const width = playerProgess.clientWidth;
+    const clickX = e.offsetX;
+    music.currentTime = (clickX / width) * music.duration;
+}
+
+playBtn.addEventListener('click', togglePlay);
+prevBtn.addEventListener('click', () => changeMusic(-1));
+nextBtn.addEventListener('click', () => changeMusic(1));
+
+music.addEventListener('ended', () => changeMusic(1));
+music.addEventListener('timeupdate',updateProgressBar);
+playerProgess.addEventListener('click', setProgressBar);
+
+loadMusic(songs[musicIndex]);
