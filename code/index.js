eff8be2cb43acac786f116c8a1178c4451c3f8c0
jordan Tomegah <tomegahjordan81@gmail.com>
2024-02-17 19:19:11 +0000

    initialisation des variables contenant des elements du DOM et ajout du dossier des chansons

---
+++ code/index.js
@@ -0,0 +1,20 @@
+const image = document.getElementById("cover"),
+title = document.getElementById("music-title"),
+artist = document.getElementById("music-artist"),
+currentTimeEl = document.getElementById("current-time"),
+durationEl = document.getElementById("duration"),
+progress = document.getElementById("progress"),
+playerProgess = document.getElementById("player-progess"),
+prevBtn = document.getElementById("prev"),
+nextBtn = document.getElementById("next"),
+playBtn = document.getElementById("play"),
+background = document.getElementById("bg-img");
+
+
+const music = new Audio();
+// 
+const songs = [
+    {
+        
+    }
+];
