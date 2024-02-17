f73d207b1cea35122c88479e951e048d257aeca3
jordan Tomegah <tomegahjordan81@gmail.com>
2024-02-17 19:40:04 +0000

    definition de l'objet music de type Audio, du tableau des données des chansons et des fonctions de playMusic et pauseMusic

--- code/index.js
+++ code/index.js
@@ -15,6 +15,52 @@ const music = new Audio();
 // 
 const songs = [
     {
-        
-    }
+        path : "../songs/The lumineers - Sleep on the floor.mp3",
+        displayName: "Sleep on the floor",
+        cover: "The Lumineers Iphone Wallpaper-Sleep on the floor #lumineersaesthetic #lumineers #sleeponthefloor #thelumimeers.jpg",
+        artist: "The Lumineers",
+    },
+    {
+        path : "../songs/The lumineers - Angela.mp3",
+        displayName: "Angela",
+        cover: "the lumineers -- Angela.jpg",
+        artist: "The Lumineers",
+    },
+    {
+        path : "../songs/The lumineers Cleopatra.mp3",
+        displayName: "Cleopatra",
+        cover: "lumineers wallpaper.jpg",
+        artist: "The Lumineers",
+    },
 ];
+
+let musicIndex = 0;
+let isPlaying = false;
+function togglePlay(){
+    if(isPlaying){
+        pauseMusic();
+    }else{
+        playMusic();
+    }
+}
+
+function playMusic(){
+    isPlaying = true;
+    //Change play button icon
+    playBtn.classList.replace("fa-play", "fa-pause");
+    // set button hover title
+    playBtn.setAttribute('title','pause');
+
+    music.play();
+}
+
+
+function pauseMusic(){
+    isPlaying = false;
+    //Change play button icon
+    playBtn.classList.replace("fa-pause", "fa-play");
+    // set button hover title
+    playBtn.setAttribute('title','play');
+
+    music.pause();
+}
